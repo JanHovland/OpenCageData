030c133c9a74fe4e31e6a06780d86e52eb18af66
Jan Hovland <jan.hovland@lyse.net>
2022-09-02 18:01:25 +0200

    Initial Commit

---
+++ OpenCageData/OpenCageDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCageDataApp.swift
+//  OpenCageData
+//
+//  Created by Jan Hovland on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenCageDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
